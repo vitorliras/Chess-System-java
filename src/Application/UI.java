ed8184c9e20464dd10bc0decf8e31a14c6e6dda2
Vitor Lira <vitorlirasti@hotmail.com>
2021-04-07 20:01:35 -0300

    CheckMate logic

--- src/Application/UI.java
+++ src/Application/UI.java
@@ -59,9 +59,14 @@ public class UI { // forma do tabuleiro (Usar o git pu algum prompt de comando, 
 		printCapturePieces(captured);
 		System.out.println();
 		System.out.println("Turn: " + chessMatch.getTurn());
+		if(!chessMatch.getCheckMate()) {
 		System.out.println("Waiting player: "+ chessMatch.getCurrentPlayer());
-		if(chessMatch.getCheck()) {
-			System.out.println("CHECK!");
+			if(chessMatch.getCheck()) {
+				System.out.println("CHECK!");
+			}
+		}else {
+			System.out.println("CHECKMATE");
+			System.out.println("WINNER: " + chessMatch.getCurrentPlayer());
 		}
 	}
 
