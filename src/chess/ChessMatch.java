144b1c42a3f3c96f400fe406d3a517bb378093a2
Vitor Lira <vitorlirasti@hotmail.com>
2021-04-07 22:03:53 -0300

    Bishop

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
+import chess.pieces.Bishop;
 import chess.pieces.King;
 import chess.pieces.Rook;
 import chess.pieces.pawn;
@@ -191,7 +192,9 @@ public class ChessMatch { // classe que serve para a s jogadas do xadrez
 	
 	private void initialSetup() { // posição inicial das peças
 		placeNewPiece('a', 1, new Rook(board, Color.WHITE));
+		placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
         placeNewPiece('e', 1, new King(board, Color.WHITE));
+        placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
         placeNewPiece('h', 1, new Rook(board, Color.WHITE));
         placeNewPiece('a', 2, new pawn(board, Color.WHITE));
         placeNewPiece('b', 2, new pawn(board, Color.WHITE));
@@ -202,9 +205,11 @@ public class ChessMatch { // classe que serve para a s jogadas do xadrez
         placeNewPiece('g', 2, new pawn(board, Color.WHITE));
         placeNewPiece('h', 2, new pawn(board, Color.WHITE));
 
-        placeNewPiece('b', 8, new Rook(board, Color.BLACK));
+        
         placeNewPiece('a', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
         placeNewPiece('e', 8, new King(board, Color.BLACK));
+        placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
         placeNewPiece('h', 8, new Rook(board, Color.BLACK));
         placeNewPiece('a', 7, new pawn(board, Color.BLACK));
         placeNewPiece('b', 7, new pawn(board, Color.BLACK));
